e0b7aa69331bb8193ddc85c3378efe08403eecb1
Lucas R Da Silva <lucasrdasilva@MacBook-Pro-de-Lucas.local>
2026-02-16 09:42:48 -0800

    corrigir 1

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ class Tarefa(BaseModel):
 @app.get("/tarefas")
 def get_tarefas():
     if not minhas_tarefas:
-        return {"message": "Não existe nenhuma tarefa!"}
-    return {"tarefas": list(minhas_tarefas.values())}
+        return {"message": "Não existe nenhuma tarefa!"}
+    return {"tarefas": [tarefa.dict() for tarefa in minhas_tarefas.values()]}
     
 @app.post("/adiciona")
 def post_tarefas(tarefa: Tarefa):
     if tarefa.nome_tarefa in minhas_tarefas:
         raise HTTPException(status_code=400, detail="Essa tarefa já existe, colega!")
-    minhas_tarefas[tarefa.nome_tarefa] = tarefa.dict()
+    minhas_tarefas[tarefa.nome_tarefa] = tarefa
     return {"message": "A tarefa foi criada com sucesso!"}
     
 @app.put("/concluir/{nome_tarefa}")
@@ -28,9 +28,9 @@ def concluir_tarefa(nome_tarefa: str):
     tarefa = minhas_tarefas.get(nome_tarefa)
     if not tarefa:
         raise HTTPException(status_code=404, detail="Essa tarefa não foi encontrada!")
-    tarefa["concluida_tarefa"] = True
+    tarefa.concluída_tarefa = True
     return {"message": "A sua tarefa foi concluída com sucesso!"}
-    
+
 
 @app.delete("/deletar/{nome_tarefa}")
 def delete_tarefa(nome_tarefa: str):
