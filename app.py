2ba9d5e18ee5d67d5d4329fa155ad3ca35ed092f
Lucas R Da Silva <lucasrdasilva@MacBook-Pro-de-Lucas.local>
2026-02-16 12:13:30 -0800

    corrigir 2

--- app.py
+++ app.py
@@ -3,38 +3,44 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-minhas_tarefas = {}
+
 
 class Tarefa(BaseModel):
     nome_tarefa: str
     descricao_tarefa: str
-    concluida_tarefa: bool
+    concluida_tarefa: bool = False
+
+minhas_tarefas = {}
+
+def encontrar_tarefa(nome):
+    return next((tarefa for tarefa in minhas_tarefas if tarefa.nome_tarefa == nome), None)
 
 @app.get("/tarefas")
 def get_tarefas():
     if not minhas_tarefas:
-        return {"message": "Não existe nenhuma tarefa!"}
-    return {"tarefas": [tarefa.dict() for tarefa in minhas_tarefas.values()]}
-    
-@app.post("/adiciona")
-def post_tarefas(tarefa: Tarefa):
-    if tarefa.nome_tarefa in minhas_tarefas:
+        return {"message": "Não existe nenhuma tarefa!"}
+    return {"tarefas": [tarefa.dict() for tarefa in minhas_tarefas]}
+
+@app.post("/tarefas")
+def post_tarefa(tarefa: Tarefa):
+    if encontrar_tarefa(tarefa.nome_tarefa):
         raise HTTPException(status_code=400, detail="Essa tarefa já existe, colega!")
-    minhas_tarefas[tarefa.nome_tarefa] = tarefa
+    minhas_tarefas.append(tarefa)
     return {"message": "A tarefa foi criada com sucesso!"}
     
-@app.put("/concluir/{nome_tarefa}")
+@app.put("/tarefas/{nome_tarefa}/concluir")
 def concluir_tarefa(nome_tarefa: str):
-    tarefa = minhas_tarefas.get(nome_tarefa)
+    tarefa = encontrar_tarefa(nome_tarefa)
     if not tarefa:
         raise HTTPException(status_code=404, detail="Essa tarefa não foi encontrada!")
-    tarefa.concluída_tarefa = True
+    tarefa.concluida_tarefa = True
     return {"message": "A sua tarefa foi concluída com sucesso!"}
 
 
-@app.delete("/deletar/{nome_tarefa}")
+@app.delete("/tarefas/{nome_tarefa}")
 def delete_tarefa(nome_tarefa: str):
-    if nome_tarefa not in minhas_tarefas:
+    tarefa = encontrar_tarefa(nome_tarefa)
+    if not tarefa:
         raise HTTPException(status_code=404, detail="Essa tarefa não foi encontrada!")
-    del minhas_tarefas[nome_tarefa]
+    minhas_tarefas.remove(tarefa)
     return {"message": "Sua tarefa foi deletada com sucesso!"}
